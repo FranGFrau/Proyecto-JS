a0a988d5261db09e16cc8cbe39b8ad71c5bf3fa1
Franco Gonzalez Frau <frangf2xxx@gmail.com>
2021-12-19 01:51:41 -0300

    simulador rehecho, empezando con el css

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 class Personajes {
-  constructor(id, asesinatos, muertes, asistencias, promedio) {
-    this.id = id;
+  constructor(asesinatos, muertes, asistencias, promedio) {
     this.asesinatos = asesinatos;
     this.asistencias = asistencias;
     this.muertes = muertes;
@@ -10,58 +9,59 @@ class Personajes {
 
 const arrayPartidas = [];
 
-function pedirPartidas() {
-  let l = prompt("Inserte su cantidad de partidas jugadas");
+/* function moduloGral() {
+  function pedirPartidas() {
+    let l = prompt("Inserte su cantidad de partidas jugadas");
 
-  while (isNaN(l) || l == null || l == "") {
-    l = prompt("Inserte un número");
-  }
-  return l;
-}
-
-let partidas = pedirPartidas();
-
-for (let vuelta = 0; vuelta != partidas; vuelta++) {
-  function pedirAsesinatos() {
-    let i = prompt("Inserte su cantidad de asesinatos");
-
-    while (isNaN(i) || i == null || i == "") {
-      i = prompt("Inserte un número");
+    while (isNaN(l) || l == null || l == "") {
+      l = prompt("Inserte un número");
     }
-    return i;
+    return l;
   }
-  function pedirMuertes() {
-    let j = prompt("Inserte su cantidad de muertes");
 
-    while (isNaN(j) || j == null || j == "") {
-      j = prompt("Inserte un número");
+  let partidas = pedirPartidas();
+
+  for (let vuelta = 0; vuelta != partidas; vuelta++) {
+    function pedirAsesinatos() {
+      let i = prompt("Inserte su cantidad de asesinatos");
+
+      while (isNaN(i) || i == null || i == "") {
+        i = prompt("Inserte un número");
+      }
+      return i;
     }
-    return j;
-  }
-  function pedirAsistencias() {
-    let k = prompt("Inserte su cantidad de asistencias");
+    function pedirMuertes() {
+      let j = prompt("Inserte su cantidad de muertes");
 
-    while (isNaN(k) || k == null || k == "") {
-      k = prompt("Inserte un número");
+      while (isNaN(j) || j == null || j == "") {
+        j = prompt("Inserte un número");
+      }
+      return j;
     }
-    return k;
+    function pedirAsistencias() {
+      let k = prompt("Inserte su cantidad de asistencias");
+
+      while (isNaN(k) || k == null || k == "") {
+        k = prompt("Inserte un número");
+      }
+      return k;
+    }
+
+    let asesinatos = pedirAsesinatos();
+    let muertes = pedirMuertes();
+    let asistencias = pedirAsistencias();
+    let promedio = (parseInt(asesinatos) + parseInt(asistencias)) / muertes;
+
+    let PartidaIngresada = new Personajes(
+      id,
+      asesinatos,
+      muertes,
+      asistencias,
+      promedio
+    );
+    arrayPartidas.push(PartidaIngresada);
   }
-
-  let id = vuelta + 1;
-  let asesinatos = pedirAsesinatos();
-  let muertes = pedirMuertes();
-  let asistencias = pedirAsistencias();
-  let promedio = (parseInt(asesinatos) + parseInt(asistencias)) / muertes;
-
-  let PartidaIngresada = new Personajes(
-    id,
-    asesinatos,
-    muertes,
-    asistencias,
-    promedio
-  );
-  arrayPartidas.push(PartidaIngresada);
-}
+} */
 
 /* for (let contador = 0; contador != arrayPartidas.length; contador++) {
   let promedio =
@@ -75,14 +75,36 @@ console.log(arrayPartidas);
 
 /* interactuar con el dom */
 
-const container = document.getElementById("2doContainer");
+let elFormulario = document.getElementById("containerInputs");
+elFormulario.addEventListener("submit", sacarCalculoPromedio);
 
-arrayPartidas.forEach((Personajes) => {
-  let displayInfo = document.createElement("div");
-  displayInfo.innerHTML = `<div>Partida ${Personajes.id} </div>
-  <div>Asesinatos ${Personajes.asesinatos}</div>
+function sacarCalculoPromedio(e) {
+  e.preventDefault();
+  let formulario = e.target;
+  let asesinatos = formulario.children[1].value;
+  let muertes = formulario.children[3].value;
+  let asistencias = formulario.children[5].value;
+  let promedio = (parseInt(asesinatos) + parseInt(asistencias)) / muertes;
+
+  let PartidaIngresada = new Personajes(
+    asesinatos,
+    muertes,
+    asistencias,
+    promedio
+  );
+  arrayPartidas.push(PartidaIngresada);
+
+  const container = document.getElementById("2doContainer");
+
+  arrayPartidas.forEach((Personajes) => {
+    let displayInfo = document.createElement("div");
+    displayInfo.classList.add("boxInfo");
+    displayInfo.innerHTML = `<div>Asesinatos ${Personajes.asesinatos}</div>
   <div>Muertes ${Personajes.muertes}</div>
   <div>Asistencias ${Personajes.asistencias}</div>
   <div>Promedio ${Personajes.promedio}</div>`;
-  container.appendChild(displayInfo);
-});
+    container.appendChild(displayInfo);
+  });
+
+  arrayPartidas.splice(0, 1);
+}
