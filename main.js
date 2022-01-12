11ad33e5da9affe8e6b14faff59287b93e1e2ade
Franco Gonzalez Frau <frangf2xxx@gmail.com>
2022-01-12 16:44:56 -0300

    tabla funcionando correctamente

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 class Personajes {
-  constructor(asesinatos, muertes, asistencias, promedio) {
+  constructor(asesinatos, muertes, asistencias, promedio, id) {
     this.asesinatos = asesinatos;
     this.asistencias = asistencias;
     this.muertes = muertes;
     this.promedio = promedio;
+    this.id = id;
   }
 }
 
 const arrayPartidas = [];
-
+const arrayEntrarTop = [];
 const arrayTop = [
   { id: 1, promedio: 9 },
   { id: 2, promedio: 5 },
@@ -38,39 +39,53 @@ function sacarCalculoPromedio(e) {
   let muertes = formulario.children[3].value;
   let asistencias = formulario.children[5].value;
   let promedio = (parseInt(asesinatos) + parseInt(asistencias)) / muertes;
+  promedio = promedio.toFixed(1);
 
-  let PartidaIngresada = new Personajes(
+  let partidaIngresada = new Personajes(
     asesinatos,
     muertes,
     asistencias,
     promedio
   );
-  arrayPartidas.push(PartidaIngresada);
+  arrayPartidas.push(partidaIngresada);
 
   arrayPartidas.forEach((Personajes) => {
     $("#2doContainer")
       .append(`<div class="boxInfo"><div class="boxEstilo">Asesinatos ${Personajes.asesinatos}</div>
   <div class="boxEstilo">Muertes ${Personajes.muertes}</div>
   <div class="boxEstilo">Asistencias ${Personajes.asistencias}</div>
-  <div class="boxEstilo">Promedio ${Personajes.promedio}</div></div>`);
+  <div class="boxEstilo" id="promedio">Promedio ${Personajes.promedio}</div></div>`);
   });
+
+  arrayTop.push({
+    id: arrayTop.length + 1,
+    promedio: partidaIngresada.promedio,
+  });
+
+  actualizarTablaPosiciones();
 
   arrayPartidas.splice(0, 1);
 }
 
-arrayTop.sort(function (a, b) {
-  return b.promedio - a.promedio;
-});
+//
 
-arrayTop.forEach((Top) => {
-  let posicion = arrayTop.indexOf(Top);
-  $("#4toContainer")
-    .append(`<div class="boxInfo"><div class="boxPosicion">Posicion ${
-    posicion + 1
-  }</div>
-  <div class="boxEstilo">Promedio ${Top.promedio}</div></div>`);
-});
+const actualizarTablaPosiciones = () => {
+  arrayTop.sort(function (a, b) {
+    return b.promedio - a.promedio;
+  });
 
+  $("#4toContainer")[0].innerHTML = "";
+  arrayTop.forEach((Top) => {
+    let posicion = arrayTop.indexOf(Top);
+    $("#4toContainer")
+      .append(`<div class="boxInfo"><div class="boxPosicion">Posicion ${
+      posicion + 1
+    }</div>
+    <div class="boxEstilo">Promedio ${Top.promedio}</div></div>`);
+  });
+};
+
+actualizarTablaPosiciones();
 /* sliders banner */
 
 $("#sliderBanner1").append(`
@@ -111,4 +126,3 @@ $("#sliderBanner3").hover(() => {
 $("#sliderBanner3").mouseleave(() => {
   $("#sliderTransparente3").slideDown("fast");
 });
-
